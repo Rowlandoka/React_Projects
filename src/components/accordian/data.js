f1c88d1b221019fd73db7426851d2af1866d4e45
Rowland Oka <rowlandoka@gmail.com>
2024-02-06 20:55:10 +0100

    complete accordian project.

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+	{
+		id: '1',
+		questions: 'What are accordian components?',
+		answers:
+			'Accordian components are a way to show and hide content. They are useful for hiding content that is not immediately relevant to the user.',
+	},
+	{
+		id: '2',
+		questions: 'What are the benefits of using accordian components?',
+		answers:
+			'Accordian components are a great way to save space on a page. They allow you to show and hide content as needed.',
+	},
+	{
+		id: '3',
+		questions: 'How do you create an accordian component?',
+		answers:
+			'You can create an accordian component by using HTML, CSS, and JavaScript. You can also use a library like React or Vue to create an accordian component.',
+	},
+	{
+		id: '4',
+		questions: 'What are some examples of accordian components?',
+		answers:
+			'Some examples of accordian components include the FAQ section on a website, the settings menu on a mobile app, and the navigation menu on a website.',
+	},
+];
+
+export default data;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,58 @@
+//Single select accordian
+//multiple select accordian
+
+import { useState } from 'react';
+import data from './data';
+import './styles.css';
+
+export default function Accordian() {
+	const [selected, setSelected] = useState(null);
+	const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+	const [multiple, setMultiple] = useState([]); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+	function handleSingleSelection(getCurrentId) {
+		setSelected(getCurrentId === selected ? null : getCurrentId);
+	}
+
+	function handleMultiSelection(getCurrentId) {
+		let cpyMultiple = [...multiple];
+		const findIndex = cpyMultiple.indexOf(getCurrentId);
+
+		if (findIndex === -1) cpyMultiple.push(getCurrentId);
+		else cpyMultiple.splice(findIndex, 1);
+		setMultiple(cpyMultiple);
+	}
+
+	return (
+		<div className='wrapper'>
+			<button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>
+				Enable Multi Selection
+			</button>
+			<div className='accordian'>
+				{data && data.length > 0 ? (
+					data.map((dataItem) => (
+						<div key={dataItem.id} className='item'>
+							<div
+								onClick={
+									enableMultiSelect
+										? () => handleMultiSelection(dataItem.id)
+										: () => handleSingleSelection(dataItem.id)
+								}
+								className='title'
+							>
+								<h3>{dataItem.questions}</h3>
+								<span>+</span>
+							</div>
+							{selected === dataItem.id ||
+							multiple.indexOf(dataItem.id) !== -1 ? (
+								<div className='content'>{dataItem.answers}</div>
+							) : null}
+						</div>
+					))
+				) : (
+					<div>No data found</div>
+				)}
+			</div>
+		</div>
+	);
+}
